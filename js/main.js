0370d8543e9e0df5a83c3ed16c3c169ed5b9de78
Vanessa Valladares <100142668+vivq22@users.noreply.github.com>
2025-01-29 21:49:41 -0600

    Update styles and enhance contact form validation; add skills section to index.html

--- js/main.js
+++ js/main.js
@@ -5,3 +5,61 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+
+const audio = document.createElement("audio");
+audio.preload = "auto";
+audio.src = "../img/mp3/Strawberry.mp3";
+audio.autoplay = true;
+audio.loop = true;
+audio.volume = 0.3;
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (name === "") {
+        alert("Por favor, ingresa tu nombre completo.");
+        return;
+    }
+
+    if (email === "") {
+        alert("Por favor, ingresa tu correo electrónico.");
+        return;
+    }
+
+    const emailPattern = /^[^@]+@[^@]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Por favor, ingresa un correo electrónico válido.");
+        return;
+    }
+
+    if (phone === "") {
+        alert("Por favor, ingresa tu número de teléfono.");
+        return;
+    }
+
+    const phonePattern = /^[0-9]{10}$/;
+    if (!phonePattern.test(phone)) {
+        alert("Por favor, ingresa un número de teléfono válido con 10 dígitos.");
+        return;
+    }
+
+    if (subject === "") {
+        alert("Por favor, ingresa un asunto.");
+        return;
+    }
+
+    if (message === "") {
+        alert("Por favor, ingresa tu mensaje.");
+        return;
+    }
+
+    alert("¡Formulario enviado correctamente!");
+
+});
